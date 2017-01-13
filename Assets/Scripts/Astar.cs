9dc8ad94864745e8a94aca85e801c8555847da28
WolfFire404 <reillyvas9@live.nl>
2017-01-13 11:21:47 +0100

    walkable

--- Assets/Scripts/Astar.cs
+++ Assets/Scripts/Astar.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Astar : MonoBehaviour {
+	[SerializeField]
+	private int width = 20, height = 20;
+	[SerializeField]
+	private LayerMask mask;
 
-	// Use this for initialization
+	private Grid grid;
+	private Walkable walkable;
+
 	void Start () {
-		
+		grid = new Grid (width, height);
+		walkable = new Walkable (grid, mask);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		walkable.Reset ();
+	}
+
+	void OnDrawGizmos() {
+		if (grid != null) {
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					Node n = grid.GetNode (x, y);
+					if (n.IsWalkable) {
+						Gizmos.color = Color.green;
+					} else {
+						Gizmos.color = Color.red;
+					}
+
+					Gizmos.DrawCube (n.Position, Vector3.one);
+				}
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/Walkable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walkable 
+{
+	private Grid grid;
+	private LayerMask mask;
+
+	public Walkable (Grid grid, LayerMask mask){
+		this.grid = grid;
+		this.mask = mask;
+
+		Reset ();
+	}
+
+	public void Reset() {
+		for (int x = 0; x < grid.Width; x++) {
+			for (int y = 0; y < grid.Height; y++) {
+				bool isWalkable = !Physics.CheckSphere(new Vector2(x, y), .45f, mask);
+				grid.GetNode (x, y).IsWalkable = isWalkable;
+			}
+		}
+	}
+}
